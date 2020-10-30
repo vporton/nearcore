188e44c2a2b35a0b69c42368816e30fcbffabf95
Alex Kouprin <kpr@nearprotocol.com>
2020-10-30 12:52:44 +0000

    init eth contracts

--- pytest/tests/bridge/simple.py
+++ pytest/tests/bridge/simple.py
@@ -5,9 +5,8 @@ sys.path.append('lib')
 from cluster import start_cluster
 from bridge import start_ganache, start_bridge
 
-#ganache = start_ganache()
-bridge = start_bridge()
 nodes = start_cluster(2, 0, 4, None, [], {})
+ganache = start_ganache()
 
 time.sleep(3)
 status = nodes[0].get_status()
@@ -15,7 +14,12 @@ print(status)
 status = nodes[1].get_status()
 print(status)
 
-bridge.init_near_contracts()
+bridge = start_bridge()
 
+print('EPIC')
+status = nodes[0].get_status()
+print(status)
+status = nodes[1].get_status()
+print(status)
 while True:
     pass
